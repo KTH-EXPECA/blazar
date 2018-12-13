bcf87f4f532d1aee393ae425ca1cc485e7ad1d69
jakecoll <jakecoll@uchicago.edu>
2018-12-13 13:17:08 -0600

    Add on_start mechanism to host plugin
    This adds a new 'on_start' behavior that can be customized on a lease.
    Currently there is only one value, 'orchestration:<stack_id>', which
    will trigger the launch of a Heat stack with the given ID when the lease
    starts. The reservation_id will be passed to the Heat stack as a
    parameter.

--- blazar/utils/trusts.py
+++ blazar/utils/trusts.py
@@ -35,7 +35,7 @@ def create_trust():
     ctx = context.current()
     trust = client.trusts.create(trustor_user=ctx.user_id,
                                  trustee_user=trustee_id,
-                                 impersonation=False,
+                                 impersonation=True,
                                  role_names=ctx.roles,
                                  project=ctx.project_id)
     return trust
