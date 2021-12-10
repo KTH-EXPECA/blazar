a578cfbae97955b354f2a5cf076ad6fafe0f7bb1
Mark Powers <markpowers@uchicago.edu>
2021-12-10 17:11:36 +0000

    Fix manager singleton instance
    Change-Id: I3ce9dc288297dcc1debf50fe9030bb4fd517c591

--- blazar/cmd/manager.py
+++ blazar/cmd/manager.py
@@ -32,9 +32,12 @@ from blazar.utils import service as service_utils
 
 
 class ManagerServiceSingleton:
-    _instance = manager_service.ManagerService()
+    _instance = None
 
     def __new__(self, resource_type=None):
+        if not ManagerServiceSingleton._instance:
+            ManagerServiceSingleton._instance = \
+                manager_service.ManagerService()
         if resource_type:
             return partial(
                 ManagerServiceSingleton._instance.call, resource_type)
